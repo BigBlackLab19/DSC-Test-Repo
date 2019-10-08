35c8369b3976f6e3ccf5cc00f505e356155342dc
Thomas Garcia <trgarcia1@up.edu.ph>
2019-10-09 01:07:29 +0800

    add questiion

--- GitDSC.c
+++ GitDSC.c
@@ -5,6 +5,41 @@ int main()
 {
     printf("Hello DSC!");
     printf("This is your Intro to Git!");
+
+    char str[SIZE];
+	int vctr = 0;
+	int ctr = 0;
+
+	printf("Input: ");
+	scanf("%[^\n]s", str);
+
+	while (str[ctr] != '\0')
+	{
+		if (str[ctr] == 'a' || str[ctr] == 'A')
+		{
+			vctr++;
+		}
+		else if (str[ctr] == 'e' || str[ctr] == 'E')
+		{
+			vctr++;
+		}
+		else if (str[ctr] == 'i' || str[ctr] == 'I')
+		{
+			vctr++;
+		}
+		else if (str[ctr] == 'o' || str[ctr] == 'O')
+		{
+			vctr++;
+		}
+		else if (str[ctr] == 'u' || str[ctr] == 'U')
+		{
+			vctr++;
+		}
+		ctr++;
+	}
+
+	printf("Answer", vctr);
+	
 return 0;
 }
 
